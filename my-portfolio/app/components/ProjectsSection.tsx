339029838fa70d1acdc435e3d6873e87b6b3dbbc
AntonioLanderos <a00574000@tec.mx>
2025-01-23 22:34:08 -0600

    Add: project functionality and images

--- my-portfolio/app/components/ProjectsSection.tsx
+++ my-portfolio/app/components/ProjectsSection.tsx
@@ -1,10 +1,29 @@
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
+import Image from 'next/image';
 
 const projects = [
-  { id: 1, title: 'Project 1', description: 'A full-stack e-commerce solution' },
-  { id: 2, title: 'Project 2', description: 'A React-based task organizer' },
-  { id: 3, title: 'Project 3', description: 'An animated portfolio site' },
-]
+  {
+    id: 1,
+    title: 'Project 1',
+    description: 'A basic functional interface of a basketball scoreboard, using HTML, CSS and Js',
+    image: '/images/bball-scoreboard.png',
+    link: 'https://basketball-scoreboard-iota.vercel.app',
+  },
+  {
+    id: 2,
+    title: 'Project 2',
+    description: 'A React-based task organizer',
+    image: '/images/under-construction.png',
+    link: 'https://www.jimdo.com/blog/under-construction-page/',
+  },
+  {
+    id: 3,
+    title: 'Project 3',
+    description: 'An animated portfolio site',
+    image: '/images/under-construction.png',
+    link: 'https://www.jimdo.com/blog/under-construction-page/',
+  },
+];
 
 export default function ProjectsSection() {
   return (
@@ -15,18 +34,29 @@ export default function ProjectsSection() {
           <motion.div
             key={project.id}
             className="p-6 rounded-lg bg-gray-800 shadow-lg hover:shadow-2xl transition-shadow duration-300 relative overflow-hidden group"
-            whileHover={{ scale: 1.05 }}
+            whileHover={{ scale: 1.05, cursor: 'pointer' }}
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
+            onClick={() => window.open(project.link)}
           >
             <div className="absolute inset-0 bg-gradient-to-r from-blue-600 to-purple-600 opacity-0 group-hover:opacity-50 transition-opacity duration-300" />
             <h3 className="text-xl font-semibold mb-2 relative z-10">{project.title}</h3>
             <p className="text-gray-300 relative z-10">{project.description}</p>
+            <div className="pt-8 relative w-full h-48 overflow-hidden rounded-lg">
+              {/* Wrapper div for the blur effect */}
+              <div className="absolute inset-0 backdrop-blur-sm group-hover:backdrop-blur-none transition-all duration-300">
+                <Image
+                  src={project.image}
+                  alt={project.title}
+                  fill // This makes the image fill the container
+                  className="h-auto max-w-lg mx-auto"
+                />
+              </div>
+            </div>
           </motion.div>
         ))}
       </div>
     </section>
-  )
+  );
 }
-
